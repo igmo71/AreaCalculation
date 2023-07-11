14d903d88100b68f1b7aa0cbf8884056deb1aedf
Igor Mogilnitskiy <igmo71@mail.ru>
2023-07-11 18:06:59 +0300

    001 Add Circle Tests.

---
+++ AreaCalc.Tests/CircleTests.cs
@@ -0,0 +1,73 @@
+﻿using AreaCalc.Lib;
+using AreaCalc.Lib.Shapes;
+
+namespace AreaCalc.Tests
+{
+    public class CircleTests
+    {
+        [Fact]
+        public void AreaCalculate_Success()
+        {
+            // Arrange
+            Circle circle = new() { Radius = 5.64190 };
+            // Act
+            var area = Area.Calculate<Circle>(circle);
+            // Assert
+            Assert.StrictEqual(expected: 100, actual: Math.Round(area, 3));
+        }
+
+        [Fact]
+        public void AreaCalculateByName_Success()
+        {
+            // Arrange
+            string shapeName = "Circle";
+            double radius = 5.64190;
+            // Act
+            var area = Area.Calculate(shapeName, radius);
+            // Assert
+            Assert.StrictEqual(expected: 100, actual: Math.Round(area, 3));
+        }
+
+        [Fact]
+        public void CircleInit_FailOnNegativeParameters()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.ThrowsAny<AppException>(() => new Circle() { Radius = -5 }); // -5 is negative parameter
+        }
+
+        [Fact]
+        public void AreaCalculate_FailOnMissMatchParameters()
+        {
+            // Arrange
+            string shapeName = "Circle";
+            double[] measures = { 3.0, 4.0 }; // Must be only One parameter
+            // Act
+            // Assert
+            Assert.ThrowsAny<AppException>(() => Area.Calculate(shapeName, measures));
+        }
+
+        [Fact]
+        public void AreaCalculate_FailOnParametersIsNull()
+        {
+            // Arrange
+            string shapeName = "Circle";
+            // Act
+            // Assert
+            Assert.Throws<AppArgumentNullException>(() => Area.Calculate(shapeName, null!)); // Measures for shapes must be present
+        }
+
+        [Fact]
+        public void AreaCalculate_FailOnShapeDefinition()
+        {
+            // Arrange
+            string shapeName = "Cube"; // Cube is not implemented shape currently
+            double[] measures = { 5.0 };
+            // Act
+            var area = Area.Calculate(shapeName, measures);
+            // Assert
+            Assert.StrictEqual(expected: 0, actual: area);
+        }
+    }
+}
